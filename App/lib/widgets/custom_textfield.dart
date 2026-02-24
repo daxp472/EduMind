93cb2ce0c59326f5cc237919e47c311f2ba48aa3
daxp472 <daxpatel.cg@gmail.com>
2026-02-24 23:50:36 +0530

    feat: create reusable custom textfield widget with validation

---
+++ App/lib/widgets/custom_textfield.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final TextEditingController controller;
+  final String label;
+  final IconData? prefixIcon;
+  final bool isPassword;
+  final String? Function(String?)? validator;
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.label,
+    this.prefixIcon,
+    this.isPassword = false,
+    this.validator,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      obscureText: isPassword,
+      validator: validator,
+      style: const TextStyle(color: Colors.white, fontSize: 15),
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: prefixIcon != null ? Icon(prefixIcon, size: 20, color: Colors.white70) : null,
+      ),
+    );
+  }
+}
