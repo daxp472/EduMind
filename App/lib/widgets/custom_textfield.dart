9f63ce52750cb8bf97780237943985af8013f5d4
daxp472 <daxpatel.cg@gmail.com>
2026-03-03 15:25:38 +0530

    refactor: update UI components and unit tests for premium build

--- App/lib/widgets/custom_textfield.dart
+++ App/lib/widgets/custom_textfield.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import '../core/app_colors.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   final TextEditingController controller;
   final String label;
   final IconData? prefixIcon;
   final bool isPassword;
   final String? Function(String?)? validator;
+  final TextInputType keyboardType;
 
   const CustomTextField({
     super.key,
@@ -14,19 +16,73 @@ class CustomTextField extends StatelessWidget {
     this.prefixIcon,
     this.isPassword = false,
     this.validator,
+    this.keyboardType = TextInputType.text,
   });
 
   @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  bool _obscureText = true;
+  bool _isFocused = false;
+
+  @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      controller: controller,
-      obscureText: isPassword,
-      validator: validator,
-      style: const TextStyle(color: Colors.white, fontSize: 15),
-      decoration: InputDecoration(
-        labelText: label,
-        prefixIcon: prefixIcon != null ? Icon(prefixIcon, size: 20, color: Colors.white70) : null,
-      ),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 4, bottom: 8),
+          child: Text(
+            widget.label.toUpperCase(),
+            style: const TextStyle(
+              fontWeight: FontWeight.w900,
+              color: AppColors.textMuted,
+              fontSize: 9,
+              letterSpacing: 1.0,
+            ),
+          ),
+        ),
+        AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.03),
+            borderRadius: BorderRadius.circular(16),
+            border: Border.all(
+              color: _isFocused ? AppColors.primary.withOpacity(0.5) : Colors.white.withOpacity(0.05),
+              width: 1,
+            ),
+          ),
+          child: Focus(
+            onFocusChange: (hasFocus) => setState(() => _isFocused = hasFocus),
+            child: TextFormField(
+              controller: widget.controller,
+              obscureText: widget.isPassword ? _obscureText : false,
+              validator: widget.validator,
+              keyboardType: widget.keyboardType,
+              style: const TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20, vertical: 18),
+                border: InputBorder.none,
+                prefixIcon: widget.prefixIcon != null 
+                  ? Icon(widget.prefixIcon, size: 18, color: _isFocused ? AppColors.primary : AppColors.textMuted) 
+                  : null,
+                suffixIcon: widget.isPassword 
+                  ? IconButton(
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
+                        size: 18,
+                        color: AppColors.textMuted,
+                      ),
+                      onPressed: () => setState(() => _obscureText = !_obscureText),
+                    )
+                  : null,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
